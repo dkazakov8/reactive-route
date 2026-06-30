8fe84ddc1c2df920df0318a14f6a88c80763273d
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2026-06-30 22:08:04 +0300

    deps: upgrade kr-observable

--- packages/adapters/kr-observable-solid.ts
+++ packages/adapters/kr-observable-solid.ts
@@ -1,8 +1,8 @@
-import { autorun, makeObservable } from 'kr-observable';
+import { autorun, makeObservable, transaction } from 'kr-observable';
 import type { TypeAdapters } from 'reactive-route';
 
 export const adapters: TypeAdapters = {
-  batch: (cb) => cb(),
+  batch: transaction,
   autorun,
   replaceObject: (obj, newObj) => {
     // biome-ignore lint/suspicious/useGuardForIn: false
--- units/pages/preact/ErrorKrObservable.tsx
+++ units/pages/preact/ErrorKrObservable.tsx
@@ -2,6 +2,6 @@ import { observer } from 'kr-observable/preact';
 
 const Error = observer((props: { error: number }) => {
   return <>Error {props.error}</>;
-});
+}, false);
 
 export default Error;
